54a86dca99e047bede6814d591d89ce77039ef1d
mlopez323wfwf <mdlopezme@gmail.com>
2020-12-15 13:40:01 -0800

    NEW Parallel design
    Combined memRead with COMNPUTE

--- scripts/bitcoin_hash_parallel.sv
+++ scripts/bitcoin_hash_parallel.sv
@@ -6,243 +6,352 @@ module bitcoin_hash(
 	output logic [31:0] mem_write_data,
 	input logic [31:0] mem_read_data
 );
+
 // Number of NONCES + one, cuz dont feel like reindexing
-	parameter NUM_NONCES = 15;
+parameter N = 15;
 
-// SHA256 K constants
-	parameter int k[0:63] = '{
-		32'h428a2f98,32'h71374491,32'hb5c0fbcf,32'he9b5dba5,32'h3956c25b,32'h59f111f1,32'h923f82a4,32'hab1c5ed5,
-		32'hd807aa98,32'h12835b01,32'h243185be,32'h550c7dc3,32'h72be5d74,32'h80deb1fe,32'h9bdc06a7,32'hc19bf174,
-		32'he49b69c1,32'hefbe4786,32'h0fc19dc6,32'h240ca1cc,32'h2de92c6f,32'h4a7484aa,32'h5cb0a9dc,32'h76f988da,
-		32'h983e5152,32'ha831c66d,32'hb00327c8,32'hbf597fc7,32'hc6e00bf3,32'hd5a79147,32'h06ca6351,32'h14292967,
-		32'h27b70a85,32'h2e1b2138,32'h4d2c6dfc,32'h53380d13,32'h650a7354,32'h766a0abb,32'h81c2c92e,32'h92722c85,
-		32'ha2bfe8a1,32'ha81a664b,32'hc24b8b70,32'hc76c51a3,32'hd192e819,32'hd6990624,32'hf40e3585,32'h106aa070,
-		32'h19a4c116,32'h1e376c08,32'h2748774c,32'h34b0bcb5,32'h391c0cb3,32'h4ed8aa4a,32'h5b9cca4f,32'h682e6ff3,
-		32'h748f82ee,32'h78a5636f,32'h84c87814,32'h8cc70208,32'h90befffa,32'ha4506ceb,32'hbef9a3f7,32'hc67178f2
-	};
-
-// Make module
-	logic done1, start1;
-	logic [31:0] answers [0:NUM_NONCES];
-	logic [31:0] pass[0:2];
-	logic [31:0] h [0:7];
-	logic [31:0] A, B, C, D, E, F, G, H;
-	computer instcomp ( h, reset_n, clk, start1, done1, answers, pass  );
-	defparam instcomp.N = NUM_NONCES;
+// Copy from memory
+logic [31:0] w [0:15][0:N];
 
 // The clock domain for all modules will be shared
-	assign mem_clk = clk;
+assign mem_clk = clk;
+
+// SHA256 K constants
+parameter int k[0:63] = '{
+    32'h428a2f98,32'h71374491,32'hb5c0fbcf,32'he9b5dba5,32'h3956c25b,32'h59f111f1,32'h923f82a4,32'hab1c5ed5,
+    32'hd807aa98,32'h12835b01,32'h243185be,32'h550c7dc3,32'h72be5d74,32'h80deb1fe,32'h9bdc06a7,32'hc19bf174,
+    32'he49b69c1,32'hefbe4786,32'h0fc19dc6,32'h240ca1cc,32'h2de92c6f,32'h4a7484aa,32'h5cb0a9dc,32'h76f988da,
+    32'h983e5152,32'ha831c66d,32'hb00327c8,32'hbf597fc7,32'hc6e00bf3,32'hd5a79147,32'h06ca6351,32'h14292967,
+    32'h27b70a85,32'h2e1b2138,32'h4d2c6dfc,32'h53380d13,32'h650a7354,32'h766a0abb,32'h81c2c92e,32'h92722c85,
+    32'ha2bfe8a1,32'ha81a664b,32'hc24b8b70,32'hc76c51a3,32'hd192e819,32'hd6990624,32'hf40e3585,32'h106aa070,
+    32'h19a4c116,32'h1e376c08,32'h2748774c,32'h34b0bcb5,32'h391c0cb3,32'h4ed8aa4a,32'h5b9cca4f,32'h682e6ff3,
+    32'h748f82ee,32'h78a5636f,32'h84c87814,32'h8cc70208,32'h90befffa,32'ha4506ceb,32'hbef9a3f7,32'hc67178f2
+};
 
 // LOCALPARAMETERS
-	localparam IDLE = 4'h0, READMEM = 4'h1, WAITMEM = 4'h2, COMPUTE1 = 4'h3, COMPUTE2 = 4'h4, PREPMOD = 4'h5, UPDATEH = 4'h6, PREPWRT = 4'h8, WRITEMSG = 4'h7;
+localparam IDLE = 4'h0, PHASE0 = 4'h1, PREPHASE1 = 4'h2, PHASE1 = 4'h3, PREPHASE2 = 4'h4, PHASE2 = 4'h5, COMPUTE = 4'hc, PREPWRITE = 4'hd, WRITEMSG = 4'he, END = 4'hf;
+
 // REGISTERS
-	reg [3:0] present_state, next_state;
+reg [3:0] present_state, next_state;
+
+// Counter
+    int memIndex;
+    logic [0:1] M;
+
+// Internal Variables : Depends on N
+    logic  [31:0] A[0:N], B[0:N], C[0:N], D[0:N], E[0:N], F[0:N], G[0:N], H[0:N];
+    logic [31:0] h [0:7][0:N];
+    logic [31:0] hphase2 [0:7][0:N];
 // Internal Variables
-	
-	logic [31:0] w [0:15];
-// Counters
-	int memIndex, wordExpand;
-	logic M;
+    int computeCycle;
+
 // Main Logic
-	always_ff @(posedge clk) begin
-		case (present_state)
-			IDLE: begin
-				//$display("IDLING");
-				mem_we <= 0;
-				start1 <= 0;
-				mem_addr <= message_addr;
-				memIndex <= 0;
-				M <= 0;
-				wordExpand <= 1;
-				
-				h[0] <= 32'h6a09e667;
-				h[1] <= 32'hbb67ae85;
-				h[2] <= 32'h3c6ef372;
-				h[3] <= 32'ha54ff53a;
-				h[4] <= 32'h510e527f;
-				h[5] <= 32'h9b05688c;
-				h[6] <= 32'h1f83d9ab;
-				h[7] <= 32'h5be0cd19;
+    always_ff @ (posedge clk) begin
+        case( present_state )
+            IDLE: begin
+                mem_we <= 0;
+                mem_addr <= message_addr;
+                memIndex <= 0;
 
-				A <= 32'h6a09e667;
-				B <= 32'hbb67ae85;
-				C <= 32'h3c6ef372;
-				D <= 32'ha54ff53a;
-				E <= 32'h510e527f;
-				F <= 32'h9b05688c;
-				G <= 32'h1f83d9ab;
-				H <= 32'h5be0cd19;
-			end
-			WAITMEM: begin
-				if( ~M )
-					mem_addr <= message_addr + 16'd1;
-				else
-					mem_addr <= message_addr + 16'd17;
-				
-				done <= 0;
-				wordExpand <= 1;
-				
-			end
-			READMEM: begin
-				//$display("msg_addr: %h, memIndex: %d, mem_read_data: %h", mem_addr, memIndex, mem_read_data );
-				memIndex <= memIndex + 1; // Increment the index
-				mem_addr <= mem_addr + 16'd1;
-				//$display("%d, %d, %h", memIndex, mem_addr, mem_read_data);
-				
-				if  (M)
-					pass[memIndex] <= mem_read_data;
-				else
-					w[memIndex] <= mem_read_data;
+                A[0] <= 32'h6a09e667;
+                B[0] <= 32'hbb67ae85;
+                C[0] <= 32'h3c6ef372;
+                D[0] <= 32'ha54ff53a;
+                E[0] <= 32'h510e527f;
+                F[0] <= 32'h9b05688c;
+                G[0] <= 32'h1f83d9ab;
+                H[0] <= 32'h5be0cd19;
 
-				
-			
-			end
-			COMPUTE1: begin
-				//$display("Wordexpand round: %d", wordExpand);
-				if ( wordExpand > 15 && wordExpand < 64) begin
-					//$display("wordExpand[%2d]", wordExpand);
-					w[wordExpand%16] <= wordexpansion( w[(wordExpand-16)%16], w[(wordExpand-15)%16], w[(wordExpand-7)%16], w[(wordExpand-2)%16]);
-				end
-				
-				{ A, B, C, D, E, F, G, H } <= sha256_op( );
-					
-				wordExpand <= wordExpand + 1;
-				
-			end
-			UPDATEH: begin
-				//$display("updateH, h[0] = %h", h[0]+A);
-				h[0] <= h[0]+A;
-				h[1] <= h[1]+B;
-				h[2] <= h[2]+C;
-				h[3] <= h[3]+D;
-				h[4] <= h[4]+E;
-				h[5] <= h[5]+F;
-				h[6] <= h[6]+G;
-				h[7] <= h[7]+H;
+                h[0][0] <= 32'h6a09e667;
+				h[1][0] <= 32'hbb67ae85;
+				h[2][0] <= 32'h3c6ef372;
+				h[3][0] <= 32'ha54ff53a;
+				h[4][0] <= 32'h510e527f;
+				h[5][0] <= 32'h9b05688c;
+				h[6][0] <= 32'h1f83d9ab;
+				h[7][0] <= 32'h5be0cd19;
 
-				M <= 1; // PREP MEM
-				memIndex <= 0; // PREP MEM
-				if(~M )
-					mem_addr <= message_addr + 16'd16; // PREP MEM
-				else begin
-					mem_addr <= output_addr;
-					mem_we <= 1;
-				end
-			end
-			PREPMOD: begin
-				start1 <= 1;
-			end
-			COMPUTE2: begin
-				//$display("h{%h,%h,%h,%h,%h,%h,%h,%h}, pass{%h,%h,%h}", h[0], h[1], h[2], h[3], h[4], h[5], h[6], h[7], pass[0], pass[1], pass[2] );
-				
-				
-				start1 <=0;
-				if(done1) begin
-					mem_we <= 1;
-					mem_addr <= output_addr;
-					memIndex <= 0;
-				end
-			end
-			PREPWRT: begin
-				//$display("mem_we: %d, answer{%h,%h,%h}", mem_we, answers[0], answers[1], answers[2]);
-				mem_addr <= output_addr;
-				mem_write_data <= answers[memIndex];
-			end
-			WRITEMSG: begin
-				//$display("Write message: output: %d, h0: %h", mem_addr, h[0]);
-				memIndex <= memIndex + 1;
-				mem_addr <= mem_addr + 16'd1;
-				mem_write_data <= answers[memIndex+1];
-				if( memIndex == NUM_NONCES) begin
-					done <= 1;
-					mem_we <= 0;
-				end
-			end
-		endcase
-	end
+                
+            end
+            PHASE0: begin // Moves all the data into a w variable
+                M <= 0;
+                mem_addr <= mem_addr + 16'd1;
+                w[15][0] <= mem_read_data;
+                done <= 0;
 
-// Next State Logic
-	always_comb begin
-		case(present_state)
-			IDLE:
-				if( start)
-					next_state = WAITMEM;
-				else
-					next_state = IDLE;
-			WAITMEM:
-				next_state = READMEM;
-			READMEM:
-				if( memIndex == 15 )
-					next_state = COMPUTE1;
-				else if( memIndex == 2 && M )
-					next_state = PREPMOD;
-				else
-					next_state = READMEM;
-			COMPUTE1:
-				if( wordExpand == 64 )
-					next_state = UPDATEH;
-				else
-					next_state = COMPUTE1;
-			UPDATEH:
-				next_state = WAITMEM;
-			PREPMOD:
-				next_state = COMPUTE2;
-			COMPUTE2:
-				if(done1)
-					next_state = PREPWRT;
-				else
-					next_state = COMPUTE2;
-			PREPWRT:
-				next_state = WRITEMSG;
-			WRITEMSG:
-				if( memIndex == NUM_NONCES )
-					next_state = IDLE;
-				else
-					next_state = WRITEMSG;
-		endcase
-	end
+                for(int i = 0; i < 15; i++) w[i][0] <= w[i+1][0];
+                memIndex <= memIndex + 1;
+                computeCycle <= 15;
 
+                if( memIndex > 1 ) begin
+                    { A[0], B[0], C[0], D[0], E[0], F[0], G[0], H[0] } <= sha256_op( 0, memIndex-2 );
+                    //$display("A[%d]: %h", computeCycle-1, A[0]);
+                end
+                //$display("Read: %h, memIndex: %d", mem_read_data, memIndex);
+            end
+            PREPHASE1: begin
+                memIndex <= 0;
+                mem_addr <= message_addr + 12'd16;
+                M <= 1;
+
+                for( int k = 0; k <= N; k++) begin
+                    h[0][k] <= h[0][0] + A[0];  // updateH
+                    h[1][k] <= h[1][0] + B[0];
+                    h[2][k] <= h[2][0] + C[0];
+                    h[3][k] <= h[3][0] + D[0];
+                    h[4][k] <= h[4][0] + E[0];
+                    h[5][k] <= h[5][0] + F[0];
+                    h[6][k] <= h[6][0] + G[0];
+                    h[7][k] <= h[7][0] + H[0];
+
+                    A[k] <= h[0][0] + A[0]; // Update ABCD...
+                    B[k] <= h[1][0] + B[0];
+                    C[k] <= h[2][0] + C[0];
+                    D[k] <= h[3][0] + D[0];
+                    E[k] <= h[4][0] + E[0];
+                    F[k] <= h[5][0] + F[0];
+                    G[k] <= h[6][0] + G[0];
+                    H[k] <= h[7][0] + H[0];
+                end
+            end
+            PHASE1: begin
+                M <= 1; // NOT NEEDED
+                mem_addr <= mem_addr + 16'd1;
+                memIndex <= memIndex + 1;
+                computeCycle <= 15;  
+
+                
+                
+                for( int t = 0; t <= N; t++) begin
+                    if( memIndex <= 3 )
+                        w[15][t] <= mem_read_data;
+                    else if( memIndex == 4)
+                        w[15][t] <= t; // THe tth index
+                    else if( memIndex == 5)
+                        w[15][t] <= 32'h80000000;
+                    else if( memIndex == 16)
+                        w[15][t] <= 32'd640;
+                    else
+                        w[15][t] <= 0;
+
+                    if( memIndex > 1 ) begin
+                        { A[t], B[t], C[t], D[t], E[t], F[t], G[t], H[t] } <= sha256_op( t, memIndex-2 );
+                        //$display("A[%d]: %h", computeCycle-1, A[0]);
+                    end
+
+                    for(int i = 0; i < 15; i++) w[i][t] <= w[i+1][t]; // Rotate w
+                end
+
+            end
+            PREPHASE2: begin
+                memIndex <= 0;
+                mem_addr <= message_addr + 12'd16;
+                M <= 2; 
+                //$display("H[0]: %h", h[0][0] + A[0]);
+
+                for( int k = 0; k <= N; k++) begin
+                    h[0][k] <= 32'h6a09e667; // updateH
+                    h[1][k] <= 32'hbb67ae85;
+                    h[2][k] <= 32'h3c6ef372;
+                    h[3][k] <= 32'ha54ff53a;
+                    h[4][k] <= 32'h510e527f;
+                    h[5][k] <= 32'h9b05688c;
+                    h[6][k] <= 32'h1f83d9ab;
+                    h[7][k] <= 32'h5be0cd19;
+
+                    A[k] <= 32'h6a09e667; // Update ABCD...
+                    B[k] <= 32'hbb67ae85;
+                    C[k] <= 32'h3c6ef372;
+                    D[k] <= 32'ha54ff53a;
+                    E[k] <= 32'h510e527f;
+                    F[k] <= 32'h9b05688c;
+                    G[k] <= 32'h1f83d9ab;
+                    H[k] <= 32'h5be0cd19;
+
+                    // COULD BE ASSIGNED BY SHIFTING
+                    hphase2[0][k] <= h[0][k] + A[k];
+                    hphase2[1][k] <= h[1][k] + B[k];
+                    hphase2[2][k] <= h[2][k] + C[k];
+                    hphase2[3][k] <= h[3][k] + D[k];
+                    hphase2[4][k] <= h[4][k] + E[k];
+                    hphase2[5][k] <= h[5][k] + F[k];
+                    hphase2[6][k] <= h[6][k] + G[k];
+                    hphase2[7][k] <= h[7][k] + H[k];
+                end
+            end
+
+            PHASE2: begin
+                //$display("H[0]: %h", hphase2[memIndex][0]);
+                memIndex <= memIndex + 1;
+                computeCycle <= 15;
+
+                for( int t = 0; t <= N; t++) begin
+                    if( memIndex <= 8 )
+                        w[15][t] <= hphase2[memIndex-1][t];
+                    else if( memIndex == 9)
+                        w[15][t] <= 32'h80000000; // THe kth index
+                    else if( memIndex == 16)
+                        w[15][t] <= 32'd256;
+                    else
+                        w[15][t] <= 0;
+
+                    for(int i = 0; i < 15; i++) w[i][t] <= w[i+1][t];
+
+                    if( memIndex > 1 ) begin
+                        { A[t], B[t], C[t], D[t], E[t], F[t], G[t], H[t] } <= sha256_op( t, memIndex-2 );
+                        //$display("A[%d]: %h", computeCycle-1, A[0]);
+                    end
+                end                   
+
+            end
+
+            COMPUTE: begin // NEEDS MAjor optimization
+                //$display("w[%d]: %h", computeCycle-1, w[15][0]);
+                
+            
+                
+                for( int t = 0; t <= N; t++ ) begin
+                    for(int i = 0; i < 15; i++)begin
+                        w[i][t] <= w[i+1][t];
+                    end 
+                    
+                    w[15][t] <= newWt( t ); // Calculate new W[t]
+            
+                    { A[t], B[t], C[t], D[t], E[t], F[t], G[t], H[t] } <= sha256_op( t, computeCycle ); // Do a sha256 operation
+                end
+
+                computeCycle <= computeCycle + 1;
+            end
+            PREPWRITE: begin
+                mem_we <= 1;
+                mem_addr <= output_addr-16'd1; // Why minus 1? I have no idea
+                memIndex <= 0;
+
+                // for( int t = 0; t <= N; t++) begin
+                //     for( int j = 0; j <= 15; j++) begin
+                //         $display("w[%t][%d]: %h ",j, t, w[j][t]);
+                //     end
+                // end
+            end
+            WRITEMSG: begin
+                mem_addr <= mem_addr + 16'd1;
+                mem_write_data <= h[0][memIndex]+A[memIndex];
+                memIndex <= memIndex + 1;
+            end
+            END: begin
+                //for(int i = 0; i <= 7; i++) $display("h[%d]: %h", i, h[i][0]);
+                done <= 1;
+                mem_we <= 0;
+
+                // for( int b = 0; b <= N; b++ ) begin
+                //     for(int j = 0; j <= 15; j++) 
+                //         $display("w[%d][%d]: %h ",j, b, w[j][b]);
+                //     $display("------------");
+                // end
+                
+            end
+        endcase
+    end
 
 // Right Rotation
-function logic [31:0] rightrotate(input logic [31:0] x, input logic [7:0] r); begin
+    function logic [31:0] rightrotate(input logic [31:0] x, input logic [7:0] r); begin
 		rightrotate = (x >> r) | (x << (32-r));
 	end
 endfunction
 
 // Word Expansion
-function logic [31:0] wordexpansion( input logic [31:0] x16, x15, x7, x2 );
+function logic [31:0] newWt(input int n);
 	begin
 	logic [31:0] s0, s1;
-		s0 = rightrotate(x15, 7) ^ rightrotate(x15, 18) ^ (x15 >> 3);
-      s1 = rightrotate(x2, 17) ^ rightrotate(x2, 19) ^ (x2 >> 10);
-      wordexpansion = x16 + s0 + x7 + s1;
+		s0 = rightrotate(w[1][n], 7) ^ rightrotate(w[1][n], 18) ^ (w[1][n] >> 3);
+        s1 = rightrotate(w[14][n], 17) ^ rightrotate(w[14][n], 19) ^ (w[14][n] >> 10);
+        // if(M== 1 && n == 1)begin
+        //     //$display("W[%d]: %h", n, w[1][n] );
+        //     //$display("A[%d]: %h --> A[%d]: %h", p, A[n], p, tee1 + tee2 );
+        // end
+        newWt = w[0][n] + s0 + w[9][n] + s1;
 	end
 endfunction
 
 // Sha256 Operation
-function logic [255:0] sha256_op( );
+function logic [255:0] sha256_op( input int n, p);
 	begin
 	logic [31:0] s0, s1;
 	logic [31:0] maj, ch, tee1, tee2;
-		s0 = rightrotate(A,2) ^ rightrotate(A,13) ^ rightrotate(A,22);
-		s1 = rightrotate(E,6) ^ rightrotate(E,11) ^ rightrotate(E,25);
+		s0 = rightrotate(A[n],2) ^ rightrotate(A[n],13) ^ rightrotate(A[n],22);
+		s1 = rightrotate(E[n],6) ^ rightrotate(E[n],11) ^ rightrotate(E[n],25);
       
-		ch = ( E & F ) ^ ( (~E) & G );
-		maj = ( A & B ) ^ ( A & C ) ^ ( B & C );
+		ch = ( E[n] & F[n] ) ^ ( (~E[n]) & G[n] );
+		maj = ( A[n] & B[n] ) ^ ( A[n] & C[n] ) ^ ( B[n] & C[n] );
 		
 		tee2 = s0 + maj;
-		tee1 = H + s1 + ch + k[wordExpand-1] + w[(wordExpand-1)%16];
+        tee1 = H[n] + s1 + ch + k[p] + w[15][n];
+
+        if(M== 1 && n == 1)begin
+            //$display("%d: W[%d]: %h", n, p, w[15][n] );
+            //$display("A[%d]: %h --> A[%d]: %h", p, A[n], p, tee1 + tee2 );
+        end
+        
 		
-		sha256_op = { tee1 + tee2, A, B, C, D + tee1, E, F ,G };
+		sha256_op = { tee1 + tee2, A[n], B[n], C[n], D[n] + tee1, E[n], F[n] ,G[n] };
 	end
 endfunction
 
-// State Register
-	always_ff @ (posedge clk, negedge reset_n) begin
-		if( !reset_n )
-			present_state <= IDLE;
-		else
-			present_state <= next_state;
-	end
 
-endmodule: bitcoin_hash
+// Next State Logic
+always_comb begin
+    case(present_state)
+        IDLE:
+            if( start)
+                next_state = PHASE0;
+            else
+                next_state = IDLE;
+        PHASE0:
+            if(memIndex == 16)
+                next_state = COMPUTE;
+            else
+                next_state = PHASE0;
+        PREPHASE1:
+            next_state = PHASE1;
+        PHASE1:
+        if(memIndex == 16)
+            next_state = COMPUTE;
+        else
+            next_state = PHASE1;
+        PREPHASE2:
+            next_state = PHASE2;
+        PHASE2:
+        if(memIndex == 16)
+            next_state = COMPUTE;
+        else
+            next_state = PHASE2;
+        COMPUTE:
+            if( computeCycle == 63 && M == 0)
+                next_state = PREPHASE1;
+            else if( computeCycle == 63 && M == 1)
+                next_state = PREPHASE2;
+            else if( computeCycle == 63 && M == 2)
+            next_state = PREPWRITE;
+            else
+                next_state = COMPUTE;
+        PREPWRITE:
+            next_state = WRITEMSG;
+        WRITEMSG:
+        if(memIndex == 16)
+            next_state = END;
+        else
+            next_state = WRITEMSG;
+        END:
+            next_state = IDLE;
+    endcase
+end
+
+// State Register
+always_ff @ (posedge clk, negedge reset_n) begin
+    if( !reset_n )
+        present_state <= IDLE;
+    else
+        present_state <= next_state;
+end
+
+endmodule : bitcoin_hash
